90d2e94de3b75883a1bf88c45897577639b6f78b
Emre Teomete <teometemre@hotmail.com>
2021-10-22 18:37:35 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Emre Says hii!!")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+//dolarDun = "Emre"
+{
+    let dolarDun=9.10
+    console.log(dolarDun)
+}
+
+ console.log(dolarDun)
+
+ const euroDun = 11.20
+  //euroDun =11
+ console.log(euroDun)
+
+ let konutkredileri = ["EmreKredisi","Emlak Kredisi","Kamu Kredisi"]
+ //let users = getusers()
+ console.log(konutkredileri)
+
+  
+     console.log("<ul>")
+     for (let index = 0; index < konutkredileri.length; index++) {
+        console.log("<li>"+konutkredileri[index]+"</li>")
+         
+     }
+     console.log("</ul>")
+
+ 
