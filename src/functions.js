8b2c515d7bf1aeeb351cf4008c1c9e3842ba1d23
Emre Teomete <teometemre@hotmail.com>
2021-11-01 21:59:17 +0300

    1 kasım ders arası

---
+++ src/functions.js
@@ -0,0 +1,17 @@
+function addtocard(adet,pname="Elma") {
+    console.log("Spt eklendi : "+ pname + " adet : "+adet)
+}
+addtocard(10)
+addtocard(15,"karpız")
+addtocard(100,"cilek")
+
+let sayhii = ()=> {
+    console.log("Selam")
+}
+sayhii()
+
+let sayhii2 = function(){
+    console.log("Selam2")
+}
+
+sayhii2()
